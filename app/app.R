1ce42fbbe8e7b19439840c2c7342c04ee82ac52b
hkicinski <hubertkicinski01@gmail.com>
2026-01-02 14:47:28 -0600

    Refactored app.R with new modular structure. Fixed resolution bug

--- app/app.R
+++ app/app.R
@@ -1,14 +1,20 @@
+#app.R
+#RNAcross Application Entry Point
+#
+#Sources all modular components in dependency order and launches the Shiny app.
+
 #source all modular components
-source("../R/00_config.R")
-source("../R/01_constants_themes_css.R")
-source("../R/02_fct_data_process.R")
-source("../R/03_fct_query.R")
-source("../R/04_fct_visualization_core.R")
-source("../R/05_fct_visualization_heatmaps.R")
-source("../R/06_fct_visualization_tables.R")
-source("../R/07_fct_utils.R")
-source("../R/08_ui.R")
-source("../R/09_server.R")
+source("../R/01_config.R")
+source("../R/02_constants_themes.R")
+source("../R/03_utils.R")
+source("../R/04_data_io.R")
+source("../R/05_orthology_query.R")
+source("../R/06_data_process.R")
+source("../R/07_visualization_core.R")
+source("../R/08_visualization_heatmaps.R")
+source("../R/09_visualization_outputs.R")
+source("../R/10_ui.R")
+source("../R/11_server.R")
 
 #run the app
 shinyApp(ui = ui, server = server)
